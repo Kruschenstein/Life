7d8a5676d3d779465d96b132d6a415b904a694fa
tfabre <mas.tho3@yahoo.fr>
2020-05-10 17:30:32 +0200

    Setup React.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Cell from './Cell';
+
+class App extends React.Component {
+  static HEIGHT = 4;
+  static WIDTH = 4;
+
+  render() {
+    const cells = new Array(App.HEIGHT * App.WIDTH);
+    for (let i = 0; i < App.HEIGHT * App.WIDTH; ++i) {
+      cells[i] = <Cell key={i} />;
+    }
+    return (
+      <div>
+        <input id="pause" type="checkbox" />
+        <label htmlFor="pause">Pause</label>
+        <div className="container">
+          {cells}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Cell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cell.css';
+
+class Cell extends React.Component {
+    render() {
+        return (
+            <div className="item"></div>
+        );
+    }
+}
+
+export default Cell;
